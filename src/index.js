0d27f9a95411c9bd56ebe5c7571645502e0cc461
SergeyShashin <s9997910@yandex.ru>
2025-05-18 17:39:03 +0300

    React180525 updated.

--- src/index.js
+++ src/index.js
@@ -8,11 +8,16 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 
-const element = React.createElement(
-  'h1',
-  { className: 'react-welcome' },
-  'Привет! Реакт работает!',
-);
+// const element = React.createElement(
+//   'h1',
+//   { className: 'react-welcome' },
+//   'Привет! Реакт работает!',
+// );
+// const element = <h1 className='react-welcome' >'Привет! Реакт JSX работает!'</h1>;
+
+const messages = ['Привет друг!', 'Как дела?', 'Как настроение?', 'Как погода?'];
+const Message = (props) => <p>{props.message}</p>;
+const Messages = (props) => props.messages.map(msg => <Message message={msg} />);
 
 const root = createRoot(document.getElementById('root'));
-root.render(element);
+root.render(<Messages messages={messages} />);
