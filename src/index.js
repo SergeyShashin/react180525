85b3a7cadf3ca1ad10791a4989b0c04ff9761559
SergeyShashin <s9997910@yandex.ru>
2025-05-18 16:50:36 +0300

    React180525 updated.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import { User } from './user.js';
+// import { User } from './user.js';
 
-console.log('Welcome, WORLD!!!');
+// console.log('Welcome, WORLD!!!');
 
-let user = new User('Luck', 'Skywalker');
-user.sayWelcome();
+// let user = new User('Luck', 'Skywalker');
+// user.sayWelcome();
+
+import React from "react";
+import { createRoot } from 'react-dom/client';
+
+const element = React.createElement(
+  'h1',
+  { className: 'react-welcome' },
+  'Привет! Реакт работает!',
+);
+
+const root = createRoot(document.getElementById('root'));
+root.render(element);
