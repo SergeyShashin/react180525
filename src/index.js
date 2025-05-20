9dfb48337fcefb0f305b3eb1e4a9027875279c80
SergeyShashin <s9997910@yandex.ru>
2025-05-20 13:13:26 +0300

    React180525 lesson2 start.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
 // let user = new User('Luck', 'Skywalker');
 // user.sayWelcome();
 
-import React from "react";
+import React, { Component } from "react";
 import { createRoot } from 'react-dom/client';
 
 // const element = React.createElement(
@@ -15,9 +15,38 @@ import { createRoot } from 'react-dom/client';
 // );
 // const element = <h1 className='react-welcome' >'Привет! Реакт JSX работает!'</h1>;
 
-const messages = ['Привет, друг!', 'Как дела?', 'Как настроение?', 'Как погода?'];
-const Message = (props) => <p>{props.message}</p>;
-const Messages = (props) => props.messages.map((msg, idx) => <Message message={msg} key={idx} />);
+// const messages = ['Привет, друг!', 'Как дела?', 'Как настроение?', 'Как погода?'];
+// const Message = (props) => <p>{props.message}</p>;
+// const Messages = (props) => props.messages.map((msg, idx) => <Message message={msg} key={idx} />);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  state = {
+    counter: 8,
+  };
+
+
+  handleClick = (e) => {
+    this.setState(prevState => {
+      return { counter: prevState.counter + Number(e.target.dataset.operation) }
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button data-operation='-1' onClick={this.handleClick}>-</button>
+        <span>{this.state.counter}</span>
+        <button data-operation='1' onClick={this.handleClick}>+</button>
+      </div>
+    )
+  }
+}
+
 
 const root = createRoot(document.getElementById('root'));
-root.render(<Messages messages={messages} />);
+// root.render(<Messages messages={messages} />);
+root.render(<App />);
